284672c1cee6121f97c40774cd74eb0d9e765527
Rupesh kumar <rupesh18123143@gmail.com>
2024-09-05 14:28:23 +0530

    remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,7 @@ import { ParallaxProvider } from "react-scroll-parallax";
 import { AdvancedBannerTop } from "./AdvancedBanner";
 import "./App.css";
 function App() {
-  // let name: string = "Rupesh singh";
-  // let age: number = 21;
-  // let arr: string[];
-  // let arr_num: number[];
-
   
-  // type Person = {
-  //   name: string;
-  //   age: number;
-  // };
-
-  // let person: Person = {
-  //   name : "rupesh",
-  //   age : 21
-  // };
-
-  // person : Object;
   return (
     <ParallaxProvider>
     <AdvancedBannerTop />
